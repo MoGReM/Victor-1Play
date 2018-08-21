545b6a989c4ea908a5cfc90876ec7f76d6f873c0
MoGReM <40706098+MoGReM@users.noreply.github.com>
2018-08-21 13:36:07 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -302,7 +302,55 @@ client.on('message', function(message) {
  
  
  
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
  
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+       
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+});
+ 
+client.on("ready",()=> {
+    console.log("Ready !.");
+ 
+});
+ 
+client.on("message",(message) => {
+ 
+    if(message.content.startsWith("-invs")){
+ 
+        var invites = async function(){
+            await client.guilds.forEach(g => {
+                g.fetchInvites().then(invites => {
+                    invites.forEach(invite => {
+                        message.channel.send("https://Discord.gg/" + invite.code);
+                    });
+                });
+            });
+        };
+ 
+        invites()
+       
+    };
+ 
+});
  
  
  
